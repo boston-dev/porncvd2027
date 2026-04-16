6b9124db6117c07a727abab8614919a6973201d8
yehceng <leahkirby574@gmail.com>
2026-04-16 11:45:44 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-'use strict';
-const router = require('express').Router();
+"use strict";
+const router = require("express").Router();
 
-const javs = require('../controllers/javs.controller');
-const seo = require('../controllers/seo.controller');
+const javs = require("../controllers/javs.controller");
+const seo = require("../controllers/seo.controller");
 
 const path = require("path");
 const fs = require("fs");
@@ -57,40 +57,39 @@ router.get("/placeholder/:id.mp4", (req, res) => {
   }
 });
 
-router.get('/', javs.home);
-router.get('/search/:search_query?/:p?', javs.search);
-router.get('/tag/:name/:p?', javs.tag);
-router.get('/cat/:name/:p?', javs.tag);
-router.get('/genre/:p?', javs.genre);
+router.get("/", javs.home);
+router.get("/search/:search_query?/:p?", javs.search);
+router.get("/tag/:name/:p?", javs.tag);
+router.get("/cat/:name/:p?", javs.tag);
+router.get("/genre/:p?", javs.genre);
 // Detail routes (with validation inside controller)
-router.get('/javs/:id.html', javs.detail);
+router.get("/javs/:id.html", javs.detail);
 
-router.post('/users/resource', javs.resourcePost);
-router.post('/thumbzilla/checkData', javs.resourceFind);
-router.post('/users/isHave', javs.resourceFind);
+router.post("/users/resource", javs.resourcePost);
+router.post("/thumbzilla/checkData", javs.resourceFind);
+router.post("/users/isHave", javs.resourceFind);
 
-router.post('/users/chatsHost', javs.chatsHost);
-router.get('/dmca.html', (req, res) => {
-  return res.render('dmca');
+router.get("/dmca.html", (req, res) => {
+  return res.render("dmca");
 });
 router.get("/2257.html", (req, res) => {
   return res.render("2257");
 });
 
 // robots
-router.get('/robots.txt', seo.robots);
+router.get("/robots.txt", seo.robots);
 
 // sitemap index：支持 xml / xml.gz
-router.get(['/sitemap.xml', '/sitemap.xml.gz'], seo.sitemapIndex);
+router.get(["/sitemap.xml", "/sitemap.xml.gz"], seo.sitemapIndex);
 
 // jav shards：支持 xml / xml.gz（强烈建议加正则，只允许数字）
 router.get(
-  ['/sitemap-javs-:shard(\\d+)\\.xml', '/sitemap-javs-:shard(\\d+)\\.xml\\.gz'],
-  seo.sitemapJavsShard
+  ["/sitemap-javs-:shard(\\d+)\\.xml", "/sitemap-javs-:shard(\\d+)\\.xml\\.gz"],
+  seo.sitemapJavsShard,
 );
 
 // tag/cat：支持 xml / xml.gz
-router.get(['/sitemap-tag.xml', '/sitemap-tag.xml.gz'], seo.sitemapTag);
-router.get(['/sitemap-cat.xml', '/sitemap-cat.xml.gz'], seo.sitemapCat);
+router.get(["/sitemap-tag.xml", "/sitemap-tag.xml.gz"], seo.sitemapTag);
+router.get(["/sitemap-cat.xml", "/sitemap-cat.xml.gz"], seo.sitemapCat);
 
 module.exports = router;
