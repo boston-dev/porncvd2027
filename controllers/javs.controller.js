4ca3e0adbadae7b1ea64f4dd5b33df15065342dc
YECHENG-D <cheng@qq.com>
2026-07-06 23:23:31 +0800

    fix

--- controllers/javs.controller.js
+++ controllers/javs.controller.js
@@ -55,7 +55,7 @@ const slectConfig = {
   type: 1,
 };
 const queryFirt = { disable: { $ne: 1 } };
-const REALTIME_MAX_PAGE = 2;
+const REALTIME_MAX_PAGE = 12;
 exports.search = [
   searchLimiter,
   asyncHandler(async (req, res) => {
